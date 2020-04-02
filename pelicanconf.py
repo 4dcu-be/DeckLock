dbb4dc3cb84f7542ac47329823303dfd367c5e22
Sebastian Proost <sebastian.proost@gmail.com>
2020-04-02 22:28:09 +0200

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+AUTHOR = "Sebastian Proost"
+SITENAME = "DeckLock"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = "Europe/Paris"
+
+DEFAULT_LANG = "en"
+
+THEME = "theme"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# We don't need any of these pages
+AUTHORS_SAVE_AS = False
+TAGS_SAVE_AS = False
+CATEGORIES_SAVE_AS = False
+ARCHIVES_SAVE_AS = False
+
+AUTHOR_SAVE_AS = False
+TAG_SAVE_AS = False
+CATEGORY_SAVE_AS = False
+
+ARTICLE_URL = "posts/{slug}/"
+ARTICLE_SAVE_AS = "posts/{slug}/index.html"
+PAGE_URL = "pages/{slug}/"
+PAGE_SAVE_AS = "pages/{slug}/index.html"
+
+# Add the assets folder to the static data
+STATIC_PATHS = ["assets"]
+
+# Configure plugins
+PLUGIN_PATHS = ["plugins"]
+PLUGINS = ["keyforge"]
+
+KEYFORGE_ENABLED = True
+
+KEYFORGE_PATH = "data"
+KEYFORGE_ASSETS_PATH = "assets/keyforge"
+
+KEYFORGE_DECK_SAVE_AS = "keyforge/{slug}.html"
+KEYFORGE_DECKS_SAVE_AS = "keyforge.html"
+
+DOK_API_KEY = os.getenv("DOK_API_KEY", None)
+
+# When set to true, external links to KeyForge image will be used
+USE_EXTERNAL_LINKS = False
+STATIC_EXCLUDES = []
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
---
+++ publishconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
+import os
+import sys
+
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+# If your site is available via HTTPS, make sure SITEURL begins with https://
+# Don't include a / at the end
+SITEURL = "https://4dcu.be/DeckLock"
+RELATIVE_URLS = False
+
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+
+DELETE_OUTPUT_DIRECTORY = False
+
+
+USE_EXTERNAL_LINKS = True
+STATIC_EXCLUDES = ['assets/keyforge']
+
+# Following items are often useful when publishing
+
+# DISQUS_SITENAME = ""
+# GOOGLE_ANALYTICS = ""
