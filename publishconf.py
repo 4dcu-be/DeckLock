5c3f2ef59df4db5441b27dffead7d0785fefb996
Sebastian Proost <sebastian.proost@gmail.com>
2021-10-02 08:21:59 +0200

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -12,7 +12,7 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 # Don't include a / at the end
-SITEURL = "https://4dcu.be/DeckLock"
+SITEURL = "https://sebastian.proost.science/DeckLock"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = None
